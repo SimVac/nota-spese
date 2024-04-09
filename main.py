c5237f3fab868790d4c36cd12fc4be775242736e
VACCA SIMONE <71303589@castelli.local>
2024-04-09 09:48:03 +0200

    Aggiunge templates, requirements, templates, templates, main, pene

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3 as sq
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":

acfd0f8bd8fc20069685425682dbb47764ec22b6
SimVac <simonevacca932@gmail.com>
2024-04-16 09:35:36 +0200

    password

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, redirect, session, request
 import settings
 import sqlite3 as sq
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = settings.SECRET_KEY
@@ -18,17 +19,28 @@ def index():
 
 @app.route("/login")
 def login():
-    if session["logged"]:
-        return redirect("/")
-    else:
+    if "logged" not in session or not session["logged"]:
         return render_template("login.html")
+    else:
+        return redirect("/")
 
 
 @app.post("/login")
 def verify_login():
-    session["logged"] = True
-    session.modified = True
-    return redirect("/")
+    password = request.form["password"]
+    hash_object = hashlib.sha256()
+    hash_object.update(password.encode())
+    hash_password = hash_object.hexdigest()
+    data = [request.form["username"], hash_password]
+    con = sq.connect("data.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM Utente WHERE username=? AND password=?", data)
+    if len(res.fetchall()) > 0:
+        session["logged"] = True
+        session.modified = True
+        return redirect("/")
+    else:
+        return redirect("/login")
 
 
 if __name__ == "__main__":

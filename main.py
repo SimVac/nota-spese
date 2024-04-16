74925492f18b67576f13080129f3d2efff5e5737
SimVac <simonevacca932@gmail.com>
2024-04-16 08:28:42 +0200

    aaaa

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
 import sqlite3 as sq
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    if session.new:
+        return redirect("/login")
+    else:
+        return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.post("/login")
+def verify_login():
+    return True
 
 
 if __name__ == "__main__":

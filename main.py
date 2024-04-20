42c460d0e13c43a4bb7b13f53eaf172161afeac2
SimVac <simonevacca932@gmail.com>
2024-04-20 20:51:47 +0200

    cose

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def index():
         session.modified = True
         return redirect("/login")
     else:
-        get_note_of_user(session["user"])
         return render_template("index.html")
 
 
@@ -41,7 +40,6 @@ def verify_login():
         session["user"] = request.form["username"]
         session["role"] = cur.execute("SELECT Ruolo.nome FROM Utente INNER JOIN Ruolo ON Ruolo.id = Utente.idRuolo WHERE username=?", [request.form["username"]]).fetchall()[0][0]
         session.modified = True
-        print(session["role"])
         con.close()
         return redirect("/")
     else:
@@ -49,18 +47,45 @@ def verify_login():
         return redirect("/login")
 
 
-
-def get_note_of_user(user):
-    con = sq.connect("data.db", detect_types=sq.PARSE_DECLTYPES |
-                             sq.PARSE_COLNAMES)
+@app.get("/api/user-notes")
+def get_notes_of_user():
+    if "logged" not in session or not session["logged"]:
+        return redirect("/login")
+    username = session["user"]
+    con = sq.connect("data.db")
     cur = con.cursor()
     res = cur.execute("SELECT Nota.*, Tipologia.descrizione as descrizione FROM Nota "
                       "INNER JOIN Utente ON Utente.id = Nota.idUtente "
                       "INNER JOIN Tipologia ON Tipologia.id = Nota.idTipologia "
-                      "WHERE username=?", [user]).fetchall()
-    res = [{'data': datetime.fromtimestamp(row[1]), 'importo': row[2], 'tipologia': row[5]} for row in res]
-    print(res)
+                      "WHERE username=?", [username]).fetchall()
+    res = [{'data': datetime.fromtimestamp(row[1]).strftime("%m/%d/%Y"), 'importo': row[2], 'tipologia': row[5]} for row in res]
     return res
+
+
+@app.get("/api/user-info")
+def get_user_info():
+    if "logged" not in session or not session["logged"]:
+        return redirect("/login")
+    username = session["user"]
+    return username
+
+
+@app.post("/api/add-user")
+def add_user():
+    if "logged" not in session or not session["logged"]:
+        return redirect("/login")
+    if session["role"] != "admin":
+        return "Need admin role!", 403
+    password = request.form["password"]
+    hash_object = hashlib.sha256()
+    hash_object.update(password.encode())
+    hash_password = hash_object.hexdigest()
+    con = sq.connect("data.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO Utente (nome, cognome, username, password, idRuolo) VALUES (?, ?, ?, ?, 2)",
+                      [request.form["nome"], request.form["cognome"], request.form["username"], hash_password])
+    con.close()
+    return redirect("/")
 
 
 if __name__ == "__main__":
